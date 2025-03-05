275eb17c7132851e47cedd8715f12cee410ed604
clamnam <clamnam@protonmail.com>
2025-03-05 12:50:47 +0000

    attempt fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home.js';
 import Page from './components/page/Page.js';
+// import Content from './components/content/Content.js';
+
 import PageWrapper from './components/page/PageWrapper.js';
+import ContentWrapper from './components/content/ContentWrapper.js';
+
 import Navbar from './components/navbar.js';
 import PageCreate from './components/page/CRUD/PageCreate.js';
+import ContentCreate from './components/content/CRUD/ContentCreate.js';
+import ContentEdit from './components/content/CRUD/ContentEdit.js';
 
 const App = () => {
   return (
     <div className='container-fluid mx-auto bg-stone-900 py-10'>
       <Router>
         {/* <div className="flex"> */}
-          {/* <div className="w-1/16 bg-gray-400 text-white p-4">
+        {/* <div className="w-1/16 bg-gray-400 text-white p-4">
             <p>Sidebar</p>
           </div> */}
           <div className="">
@@ -23,6 +29,15 @@ const App = () => {
                   <Route path=":id" element={<Page />} />
                   <Route path="/page/create"element={<PageCreate/>}></Route>                    
                   {/* <Route path="/page/edit"element={<PageEdit/>}></Route>                     */}
+
+                </Route>
+                <Route path="/content" element={<ContentWrapper />}>
+                <Route index element={<ContentCreate />}/>
+                <Route path=":id" element={<ContentEdit/> }/>
+                  {/* <Route index element={<Content />} />
+                  <Route path=":id" element={<Content />} /> */}
+                  {/* <Route path="/content/create"element={<ContentCreate/>}></Route>                     */}
+                  {/* <Route path="/content/edit"element={<ContentEdit/>}></Route>                     */}
 
                 </Route>
               </Routes>
--- src/components/page/CRUD/PageCreate.js
+++ src/components/page/CRUD/PageCreate.js
@@ -1,9 +1,7 @@
 import React, { useRef, useState } from 'react';
 // import  { PureComponent } from 'react';
-import ReactDiffViewer from 'react-diff-viewer';
-import 'tinymce/tinymce';
-import 'tinymce/themes/silver';
-import 'tinymce/icons/default';
+import ReactDiffViewer from 'react-diff-viewer-continued';
+
 
 const oldData = `
 const a = 10
@@ -17,14 +15,20 @@ if(a > 10) {
 console.log('done')
 `;
 
+
 const PageCreate = () => {
   const [diff, setDiff] = useState('');
   const editorRef = useRef(null);
 
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setDiff(<ReactDiffViewer oldValue={oldData} newValue={e.target.value} splitView={true} />);
+
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const content = editorRef.current ? editorRef.current.getContent() : '';
-    setDiff(<ReactDiffViewer oldValue={oldData} newValue={content} splitView={true} />);
 
     console.log('Submitting content:', content);
   };
@@ -32,15 +36,16 @@ const PageCreate = () => {
   return (
     <>
     {diff}
-      <form onSubmit={handleSubmit}>
-        <input type="text" />
+    <div className='text-white'>Title</div>
+    <form onSubmit={handleSubmit}>
+        <textarea onChange={onChange} type="textarea" />
         <div className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">
 
-        <button type="submit">Submit</button>
+            <button type="submit">Submit</button>
         </div>      {/* <div className='text-red-500'>{diff}</div> */}
 
-      </form>
-    </>
+    </form>
+</>
   );
 };
 
